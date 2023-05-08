4b664572cc16b2f17f4cf614985ac85c3c3685a2
David Fiel <github@dfiel.com>
2023-05-07 22:57:20 -0400

    125khz wave working

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "ook.pio.h"
+
+static const float pio_freq = 125000 * 2;
+
+void setup() {
+  // put your setup code here, to run once:
+      // Choose which PIO instance to use (there are two instances)
+    PIO pio = pio0;
+
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offset = pio_add_program(pio, &ook_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint sm = pio_claim_unused_sm(pio, true);
+
+    float div = (float)clock_get_hz(clk_sys) / pio_freq;
+
+    ook_program_init(pio, sm, offset, 0, div);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

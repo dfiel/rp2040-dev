7dbe30daa28e7aeab2116ec04018848077ced0fb
David Fiel <github@dfiel.com>
2023-05-08 22:45:42 -0400

    try to use two state machines

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include "manchester.pio.h"
+#include "ook_gen_freq.pio.h"
+#include "ook_shift.pio.h"
 
 static const float pio_freq = 125000 * 2;
 
@@ -14,21 +15,24 @@ void setup() {
     // memory. This SDK function will find a location (offset) in the
     // instruction memory where there is enough space for our program. We need
     // to remember this location!
-    uint offset = pio_add_program(pio, &manchester_program);
+    uint freq_offset = pio_add_program(pio, &ook_gen_freq_program);
+    uint shift_offset = pio_add_program(pio, &ook_shift_program);
 
     // Find a free state machine on our chosen PIO (erroring if there are
     // none). Configure it to run our program, and start it, using the
     // helper function we included in our .pio file.
-    uint sm = pio_claim_unused_sm(pio, true);
+    uint freq_sm = pio_claim_unused_sm(pio, true);
+    uint shift_sm = pio_claim_unused_sm(pio, true);
 
     float div = (float)clock_get_hz(clk_sys) / pio_freq;
 
-    manchester_program_init(pio, sm, offset, 0, div);
+    ook_gen_freq_program_init(pio, freq_sm, freq_offset, 0, div);
+    ook_shift_program_init(pio, shift_sm, shift_offset, div);
 
-    pio_sm_set_enabled(pio, sm, false);
-    pio_sm_put_blocking(pio, sm, 0b11011);
-    pio_sm_put_blocking(pio, sm, 0b00110);
-    pio_sm_set_enabled(pio, sm, true);
+    pio_sm_set_enabled(pio, freq_sm, false);
+    pio_sm_put_blocking(pio, freq_sm, 0b11011);
+    // pio_sm_put_blocking(pio, freq_sm, 0b00110);
+    pio_sm_set_enabled(pio, freq_sm, true);
 }
 
 void loop() {
